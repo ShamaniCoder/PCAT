ed915f8b324e01ac1823353d46f2208641daf931
ShamaniCoder <ertugrulaktn@gmail.com>
2022-03-12 15:47:47 +0100

    Static Files Uploaded

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function () {
-  console.log("hello");
-};
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  console.log(__dirname);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`listening on ${port}`));

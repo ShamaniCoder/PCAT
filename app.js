7c1832fca5708d17737401969f8827e405be7b41
ShamaniCoder <ertugrulaktn@gmail.com>
2022-03-14 23:07:12 +0100

    added CRUD operations

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
 const ejs = require("ejs");
+const path = require("path");
+const Photo = require("./models/Photo");
+const fs = require("fs");
 
 const app = express();
+
+//connect db
+mongoose.connect("mongodb://localhost/pcat-test-db");
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");
@@ -11,10 +18,23 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/photos/:id", async (req, res) => {
+  // res.render("about");
+  // console.log(req.params.id);
+  const photo = await Photo.findById(req.params.id);
+  res.render("photo", {
+    photo,
+  });
+});
+
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({}).sort("-dateCreated");
+  res.render("index", {
+    photos,
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
@@ -23,9 +43,25 @@ app.get("/add", (req, res) => {
   res.render("add");
 });
 
-app.post("/photos", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
+app.post("/photos", async (req, res) => {
+  // console.log(req.files.image);
+  // await Photo.create(req.body);
+  // res.redirect("/");
+
+  const uploadDir = "public/uploads";
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + "/public/uploads/" + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: "/uploads/" + uploadedImage.name,
+    });
+    res.redirect("/");
+  });
 });
 
 const port = process.env.PORT || 3000;
